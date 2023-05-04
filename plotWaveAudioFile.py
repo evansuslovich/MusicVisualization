9a1f1c230e772377009f04128f3f28e5235fcb41
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-04 18:46:03 -0400

    removed numpy and renamed file

---
+++ plotWaveAudioFile.py
@@ -0,0 +1,33 @@
+import struct
+import wave
+
+import matplotlib.pyplot as plt
+
+wavefile = wave.open('notes/piano-G3.wav', 'r')
+
+length = wavefile.getnframes()
+
+# gets all frequency levels and sets to raw
+raw = []
+for i in range(0, length):
+    waveData = wavefile.readframes(1)
+    data = struct.unpack("<h", waveData)
+    raw.append(data[0])
+
+# only mono files
+if wavefile.getnchannels() == 2:
+    print("stereo Files are not supported use mono files")
+
+# Sample rate is the number of samples per second that are taken of
+# a waveform to create a discrete digital signal
+sampleRate = wavefile.getframerate()
+
+# The time broken down
+Time = []
+for i in range(len(raw)):
+    Time.append(i / sampleRate)
+
+plt.title("Waveform of Wave File")
+plt.plot(Time, raw, color="black")
+plt.ylabel("Amplitude")
+plt.show()
