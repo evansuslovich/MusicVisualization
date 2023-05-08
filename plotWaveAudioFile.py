c1456d73c8688108897124ff513b28b7cf04ce2a
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-07 22:48:24 -0400

    removed need for numpy library however only able to account for some .wav files (mono)

--- plotWaveAudioFile.py
+++ plotWaveAudioFile.py
@@ -3,20 +3,23 @@ import wave
 
 import matplotlib.pyplot as plt
 
-wavefile = wave.open('notes/piano-G3.wav', 'r')
+wavefile = wave.open('./notes/music-loop.wav', 'r')
 
 length = wavefile.getnframes()
-
-# gets all frequency levels and sets to raw
-raw = []
-for i in range(0, length):
-    waveData = wavefile.readframes(1)
-    data = struct.unpack("<h", waveData)
-    raw.append(data[0])
 
 # only mono files
 if wavefile.getnchannels() == 2:
     print("stereo Files are not supported use mono files")
+
+# gets all frequency levels and sets to raw
+raw = []
+for i in range(0, 1):
+    waveData = wavefile.readframes(1)
+    print(waveData)
+    data = struct.unpack("<h", waveData)
+    print(data)
+    raw.append(data[0])
+
 
 # Sample rate is the number of samples per second that are taken of
 # a waveform to create a discrete digital signal
