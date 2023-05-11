2e8f386126aaa79f8d94598a9cf69b46d0895bfb
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-11 15:14:10 -0400

    better names and comments to explain what functions are doing

--- beatDetection.py
+++ beatDetection.py
@@ -20,6 +20,8 @@ def get_time(raw_data, frame_rate):
         time.append(i / frame_rate)
     return time
 
+
+# with time as x-axis
 def show_plot(raw_data, time):
     # code from https://stackoverflow.com/questions/38797934/get-the-amplitude-data-from-an-mp3-audio-files-using-python
     # create title
@@ -32,6 +34,7 @@ def show_plot(raw_data, time):
     plt.show()
 
 
+# indexes of array as x-axis
 def show_plot(raw_data):
     # code from https://stackoverflow.com/questions/38797934/get-the-amplitude-data-from-an-mp3-audio-files-using-python
     # create title
@@ -45,7 +48,7 @@ def show_plot(raw_data):
 
 
 # return an array, index 0 is max, index 1 is min.
-def get_max_and_min(raw_data):
+def get_max_and_min_in_raw_data(raw_data):
     result = []
 
     # gets the maximum and minimum values
@@ -62,33 +65,38 @@ def get_max_and_min(raw_data):
 
     return result
 
-def cleaning_y_axis(raw_data, max_val, buffer_val):
+
+# getting the values with high amplitudes
+def getting_values_between_max_value_and_buffer(raw_data, max_val, buffer_val):
     # loop through the entire array of raw data and see if it fits through buffer <= number <= max
-    index_of_max = []
+    horizontal_cleaning = []
     for i in range(len(raw_data)):
         if buffer_val <= raw_data[i] <= max_val:
-            index_of_max.append(i)
+            horizontal_cleaning.append(i)
 
-    return index_of_max
+    return horizontal_cleaning
 
-
-def cleaning_x_axis(fit_between_buffer_and_max, length_of_data):
+# what if the values between the high amplitudes are close to each other? Remove them if they're close .. extra noise
+def removing_extra_noise_horizontally(fit_between_buffer_and_max, length_of_data):
     # loop through the indexes_of_max array and removes indexes that are close horizontally
-    result_indexes_of_max = []
+    vertical_cleaning = []
     for i in range(0, len(fit_between_buffer_and_max) - 1):
         # are the indexes close to each other by a factor of 0.01 * len(raw_data) is this an accurate process?
         if (fit_between_buffer_and_max[i + 1] - fit_between_buffer_and_max[i]) > (length_of_data * 0.05):
-            result_indexes_of_max.append(fit_between_buffer_and_max[i])
+            vertical_cleaning.append(fit_between_buffer_and_max[i])
 
-    return result_indexes_of_max
+    return vertical_cleaning
 
 
+# finds the difference between consecutive indices
+# gets average of these differences
 def average_sum_of_differences(horizontal_and_vertical_cleaning):
     sum = 0
     for i in range(0, len(horizontal_and_vertical_cleaning) - 1):
         sum += horizontal_and_vertical_cleaning[i + 1] - horizontal_and_vertical_cleaning[i]
 
     return (sum / len(horizontal_and_vertical_cleaning))
+
 
 def main():
     sound_url = './songs/100-bpm-drum-loop-sample-c-sharp-key.mp3'
@@ -97,14 +105,14 @@ def main():
     raw_data = get_raw_data(sound)
     time = get_time(raw_data, sound.frame_rate)
 
-    max_and_min_values = get_max_and_min(raw_data)
+    max_and_min_values = get_max_and_min_in_raw_data(raw_data)
     max_val = max_and_min_values[0]
 
     min_val = max_and_min_values[1]
 
-    horizontal_cleaning= cleaning_y_axis(raw_data, max_val, max_val * 0.95)
+    horizontal_cleaning = getting_values_between_max_value_and_buffer(raw_data, max_val, max_val * 0.95)
 
-    horizontal_and_vertical_cleaning = cleaning_x_axis(horizontal_cleaning, len(raw_data))
+    horizontal_and_vertical_cleaning = removing_extra_noise_horizontaly(horizontal_cleaning, len(raw_data))
 
     average_sum_of_diff = average_sum_of_differences(horizontal_and_vertical_cleaning)
 
