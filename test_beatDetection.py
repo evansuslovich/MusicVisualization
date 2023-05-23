241513f671facaa9b4b1a7f52c7de6dda762aa6b
Evan Suslovich <evanjacobsuslovich@gmail.com>
2023-05-22 18:39:20 -1000

    removed uneccesary test

--- test_beatDetection.py
+++ test_beatDetection.py
@@ -3,8 +3,13 @@ from beatDetection import *
 
 
 class MyTestCase(unittest.TestCase):
-    
 
+    def test_moving_average(self):
+        arr = [1, 2, 3, 4]
+        window_size = 2
+        result = [1.5, 2.5, 3.5]
+
+        self.assertEqual(moving_average(arr, window_size), result)
 
 if __name__ == '__main__':
     unittest.main()
